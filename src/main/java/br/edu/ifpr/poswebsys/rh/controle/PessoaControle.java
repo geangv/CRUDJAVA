7ecef4c6549d08144f345415933af31f2d201c8a
Gean S <geangv@outlook.com>
2022-09-13 22:35:08 -0300

    Ultima alteracao

--- src/main/java/br/edu/ifpr/poswebsys/rh/controle/PessoaControle.java
+++ src/main/java/br/edu/ifpr/poswebsys/rh/controle/PessoaControle.java
@@ -1,9 +1,15 @@
 package br.edu.ifpr.poswebsys.rh.controle;
 
+import br.edu.ifpr.poswebsys.rh.dominio.Pessoa;
 import br.edu.ifpr.poswebsys.rh.dominio.PessoaRepositorio;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Optional;
 
 @Controller
 public class PessoaControle {
@@ -18,5 +24,36 @@ public class PessoaControle {
     public String pessoas(Model model){
         model.addAttribute("listaPessoas", pessoaRepo.findAll());
         return "rh/pessoas/index";
+    }
+
+    @GetMapping("/rh/pessoas/nova")
+    public String novaPessoa(@ModelAttribute("pessoa") Pessoa pessoa){
+        return "rh/pessoas/form";
+    }
+
+    @GetMapping("/rh/pessoas/{id}")
+    public String alterarPessoa(@PathVariable("id") long id, Model model){
+        Optional<Pessoa> pessoaOpt = pessoaRepo.findById(id);
+            if(pessoaOpt.isEmpty()){
+                throw new IllegalArgumentException("Pessoa invalida!");
+            }
+        model.addAttribute("pessoa", pessoaOpt.get());
+            return "rh/pessoas/form";
+    }
+
+    @PostMapping("/rh/pessoas/salvar")
+    public String salvarPessoas(@ModelAttribute("pessoa") Pessoa pessoa){
+        pessoaRepo.save(pessoa);
+        return "redirect:/rh/pessoas";
+    }
+
+    @GetMapping("/rh/pessoas/excluir/{id}")
+    public String excluirPessoa(@PathVariable("id") Long id){
+        Optional<Pessoa> pessoaOpt = pessoaRepo.findById(id);
+        if(pessoaOpt.isEmpty()){
+            throw new IllegalArgumentException("Pessoa Invalida");
+        }
+        pessoaRepo.delete(pessoaOpt.get());
+        return "redirect:/rh/pessoas";
     }
 }
